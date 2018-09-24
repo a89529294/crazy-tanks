a7597adcac0e63ab485e7637d9bd48683ea68db8
achang <a89529294@hotmail.com>
2018-09-24 11:01:04 +0800

    add bullet

--- js/main-scene.js
+++ js/main-scene.js
@@ -1,34 +1,50 @@
-
 let cursors;
 let tank;
+let keys;
+let bullet;
 export default class MainScene extends Phaser.Scene {
   preload() {
-   this.load.image('tank','./assets/tank.png')
+    this.load.image('tank', './assets/tank.png');
+    this.load.image('bullet', './assets/bullet.png');
 
   }
 
   create() {
-    tank = this.add.sprite(50,50,'tank').setScale(0.1,0.1)
-    tank.flipY=true;
+    tank = this.add.sprite(50, 50, 'tank').setScale(0.1, 0.1);
 
     cursors = this.input.keyboard.createCursorKeys();
+    keys = this.input.keyboard.addKeys({
+      'space': Phaser.Input.Keyboard.KeyCodes.SPACE
+    });
   }
 
-  update(){
-    if (cursors.left.isDown ) {
+  update() {
+
+    if (cursors.left.isDown) {
       tank.angle--;
-  }
-  else if (cursors.right.isDown ) {
-    tank.angle++;
-<<<<<<< HEAD
-=======
-  }
-  else if (cursors.up.isDown) {
-    var radianAngle = Math.PI*tank.angle/180;
-    tank.x += Math.sin(radianAngle);
-    tank.y += -Math.cos(radianAngle);
->>>>>>> 65f917fd0e51a22c5801c5921149dbb2569168bf
-  }
+    }
+    if (cursors.right.isDown) {
+      tank.angle++;
+    }
+    if (cursors.down.isDown) {
+      var radianAngle = Math.PI * tank.angle / 180;
+      tank.x -= Math.cos(radianAngle);
+      tank.y -= Math.sin(radianAngle);
+    }
+    if (cursors.up.isDown) {
+      var radianAngle = Math.PI * tank.angle / 180;
+      tank.x += Math.cos(radianAngle);
+      tank.y += Math.sin(radianAngle);
+    }
+    if (keys.space.isDown) {
+      bullet = this.add.sprite(tank.x, tank.y, 'bullet');
+      bullet.angle = tank.angle;
+    }
+
+    if (bullet) {
+      bullet.x += Math.cos(radianAngle);
+      bullet.y += Math.sin(radianAngle);
+    }
   }
 
 }
