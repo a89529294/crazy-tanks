56a61133294b2e52b35ab0f990ad48efcc5c42b6
achang <a89529294@hotmail.com>
2018-09-25 10:57:40 +0800

    add texts to start screen

--- js/starting-scene.js
+++ js/starting-scene.js
@@ -6,16 +6,26 @@ export default class StartingScene extends Phaser.Scene {
         this.load.image('startScreen', '../assets/startScreen.png');
         var width = this.cameras.main.width;
         var height = this.cameras.main.height;
-        var loadingText = this.make.text({
+        var titleText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 200,
+            text: 'Crazy Tanks',
+            style: {
+                font: '60px monospace',
+                fill: '#3bc5f1'
+            }
+        });
+        var enterText = this.make.text({
             x: width / 2,
             y: height / 2 - 50,
             text: 'Press Enter To Start',
             style: {
-                font: '20px monospace',
+                font: '30px monospace',
                 fill: '#ffffff'
             }
         });
-        loadingText.setOrigin(0.5, 0.5);
+        enterText.setOrigin(0.5, 0.5).depth = 1;
+        titleText.setOrigin(0.5, 0.5).depth = 1;
         this.input.keyboard.on('keydown', this.handleEnter, this);
     }
 
